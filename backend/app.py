152a04ff4cb2c57879c2b3f792c834f07bbdfb57
jems <jemsmpatel2005@gmail.com>
2025-11-07 12:40:27 +0530

    Update app.py
    email update

--- backend/app.py
+++ backend/app.py
@@ -26,14 +26,13 @@ app.config["JWT_SECRET"] = SECRET_KEY
 mongo.init_app(app)
 
 # Mail config
-app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME=MAIL_USERNAME,
-    MAIL_PASSWORD=MAIL_PASSWORD,
-)
-
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+app.config['MAIL_DEFAULT_SENDER'] = MAIL_USERNAME
 mail.init_app(app)
 
 frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
